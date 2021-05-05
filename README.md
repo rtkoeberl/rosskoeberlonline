26884227e23914dcadfc4699b4cc955e7e3c49fc
Ross Koeberl <rtkoeberl@gmail.com>
2021-05-05 14:28:44 -0500

    Archiving site

---
+++ README.md
@@ -0,0 +1,4 @@
+# rosskoeberl.online
+"It's Ross Koeberl... *online*!"
+
+*Built 9.13.2020 using HTML, SCSS, Javascript, jQuery, and Animate.css*
---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  
+  //   INTRO ANIMATION
+    setTimeout(function() {
+      $('#title')
+      .removeClass('hidden')
+      .addClass('animate__animated animate__fadeInDown')
+    }, 1000)
+     
+    setTimeout(function() {
+      $('#tagline')
+      .removeClass('hidden')
+      .addClass('animate__animated animate__fadeInLeft')
+    }, 2400);
+    
+    
+    setTimeout(function() {
+      $('#online')
+      .fadeTo('fast', 1)
+      .addClass('animate__animated animate__jackInTheBox')
+    }, 5000)
+    
+    setTimeout(function() {
+      $('#marquee')
+      .removeClass('intro')
+      .css('margin', '1em');
+    }, 8000)
+    
+    setTimeout(function() {
+      $('div.directory')
+      .fadeIn(1000);
+      $('.marquee')
+      .css('position', 'relative')
+    }, 9000)
+    
+  //  POPOUT MENUS
+    $('#digital').click(function() {
+      $('#digital-menu').fadeToggle( "slow", "linear" );
+    })
+    
+    $('#et-cetera').click(function() {
+      $('#et-cetera-menu').fadeToggle( "slow", "linear" );
+    })
+    
+    $('#writing').click(function() {
+      $('#writing-menu').fadeToggle( "slow", "linear" );
+    })
+    
+  });
